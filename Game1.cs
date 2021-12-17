ea8549ec78706054beaaa4fd7f167d3bb0aeb39f
Kenny Fournie <FourKenn744@GoTVDSB.ca>
2021-12-17 13:59:27 -0500

    Add project files.

---
+++ Game1.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace Collision_detection_with_rectangles
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        KeyboardState keyboardState;
+        MouseState mouseState;
+        
+        Texture2D pacLeftTexture;
+        Texture2D pacRightTexture;
+        Texture2D pacUpTexture;
+        Texture2D pacDownTexture;
+        Texture2D pacSleepTexture;
+        Texture2D currentPacTexture; 
+        Rectangle pacRect;
+        Texture2D exitTexture;
+        Rectangle exitRect;
+        Texture2D barrierTexture;
+        Rectangle barrierRect1, barrierRect2;
+        Texture2D coinTexture;
+        Rectangle coinRect;
+        int pacSpeed;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            pacSpeed = 3;
+            pacRect = new Rectangle(10, 10, 60, 60);
+            barrierRect1 = new Rectangle(0, 250, 350, 75);
+            barrierRect2 = new Rectangle(450, 250, 350, 75);
+            coinRect = new Rectangle(400, 50, coinTexture.Width, coinTexture.Height);
+            exitRect = new Rectangle(700, 380, 100, 100);
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            pacDownTexture = Content.Load<Texture2D>("pacDown");
+            pacUpTexture = Content.Load<Texture2D>("pacUp");
+            pacRightTexture = Content.Load<Texture2D>("pacRight");
+            pacLeftTexture = Content.Load<Texture2D>("pacLeft");
+            pacSleepTexture = Content.Load<Texture2D>("pacSleep"); 
+            barrierTexture = Content.Load<Texture2D>("rock_barrier");
+            exitTexture = Content.Load<Texture2D>("hobbit_door");
+            coinTexture = Content.Load<Texture2D>("coin");
+            currentPacTexture = pacSleepTexture;
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            if (keyboardState.IsKeyDown(Keys.Left))
+            {
+                pacRect.X -= pacSpeed;
+                currentPacTexture = pacLeftTexture;
+            }
+            if (keyboardState.IsKeyDown(Keys.Right))
+            {
+                pacRect.X += pacSpeed;
+                currentPacTexture = pacRightTexture;
+            }
+            if (keyboardState.IsKeyDown(Keys.Up))
+            {
+                pacRect.Y -= pacSpeed;
+                currentPacTexture = pacUpTexture;
+            }
+            if (keyboardState.IsKeyDown(Keys.Down))
+            {
+                pacRect.Y += pacSpeed;
+                currentPacTexture = pacDownTexture;
+            }
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            // TODO: Add your drawing code here
+
+            _spriteBatch.Begin();
+
+            _spriteBatch.Draw(barrierTexture, barrierRect1, Color.White);
+            _spriteBatch.Draw(barrierTexture, barrierRect2, Color.White);
+            _spriteBatch.Draw(exitTexture, exitRect, Color.White);
+            _spriteBatch.Draw(currentPacTexture, pacRect, Color.White);
+            _spriteBatch.Draw(coinTexture, coinRect, Color.White);
+
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Collision_detection_with_rectangles
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
